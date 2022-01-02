c34f79af10053f97c9ae40f1cb8ac794a4b207d3
Marc Rousavy <me@mrousavy.com>
2022-01-02 17:44:04 +0100

    chore(deps): Upgrade RNGH to 2.0.0

--- example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
+++ example/android/app/src/main/java/com/mrousavy/camera/example/MainActivity.java
@@ -1,9 +1,6 @@
 package com.mrousavy.camera.example;
 
 import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
@@ -20,15 +17,5 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(null);
-  }
-
-  @Override
-  protected ReactActivityDelegate createReactActivityDelegate() {
-    return new ReactActivityDelegate(this, getMainComponentName()) {
-      @Override
-      protected ReactRootView createRootView() {
-        return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
-    };
   }
 }
